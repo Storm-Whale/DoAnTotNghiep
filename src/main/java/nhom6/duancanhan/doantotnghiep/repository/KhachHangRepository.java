1f64a19d0ad81ea3d06a3e6183c859a76d3d9d72
Storm-Whale <emphong481@gmail.com>
2024-10-07 14:48:32 +0700

    crud sp

--- src/main/java/nhom6/duancanhan/doantotnghiep/repository/KhachHangRepository.java
+++ src/main/java/nhom6/duancanhan/doantotnghiep/repository/KhachHangRepository.java
@@ -4,8 +4,6 @@ import nhom6.duancanhan.doantotnghiep.entity.KhachHang;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface KhachHangRepository extends JpaRepository<KhachHang,Integer> {
 }
