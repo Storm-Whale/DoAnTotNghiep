f4534155989f46476fdb80740033803f779e1001
xuanDaok4 <130284252+xuanDaok4@users.noreply.github.com>
2024-10-09 00:02:16 +0700

    new views

--- src/main/java/nhom6/duancanhan/doantotnghiep/controller/AdminController.java
+++ src/main/java/nhom6/duancanhan/doantotnghiep/controller/AdminController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping("/")
+    @GetMapping("")
     private String sanphamindex() {
         return "/admin/indexx";
     }
--- src/main/java/nhom6/duancanhan/doantotnghiep/controller/KhachHangController.java
+++ src/main/java/nhom6/duancanhan/doantotnghiep/controller/KhachHangController.java
@@ -6,56 +6,64 @@ import nhom6.duancanhan.doantotnghiep.service.service.KhachHangService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
+@RequestMapping("/admin/khachhang")
 public class KhachHangController {
 
     @Autowired
     private KhachHangService khachHangService;
 
-    @GetMapping("/khach-hang")
-    public ResponseEntity<?> getAll() {
-        return ResponseEntity.ok(khachHangService.getAll());
+    @GetMapping("")
+    public String getAll(Model model) {
+        model.addAttribute("listKH",khachHangService.getAll());
+        return "/admin/customer/khachhang";
     }
 
     @GetMapping("/phan-trang/{pageNo}")
-    public ResponseEntity<?> phanTrang(@PathVariable(value = "pageNo") int pageNo) {
+    public String phanTrang(@PathVariable(value = "pageNo") int pageNo, Model model) {
         int pageSize = 3;
         Page<KhachHang> page = khachHangService.phanTrang(pageNo,pageSize);
         List<KhachHang> listKH = page.getContent();
-        return ResponseEntity.ok(listKH);
+        model.addAttribute("listKH",listKH);
+        return "/admin/customer/khachhang";
     }
 
     @GetMapping("/detail/{id}")
-    public ResponseEntity<?> detail(@PathVariable("id") Integer id) {
-        return ResponseEntity.ok(khachHangService.detail(id));
+    public String detail(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("detailKH",khachHangService.detail(id));
+        model.addAttribute("listKH",khachHangService.getAll());
+        return "/admin/customer/khachhang";
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> add(@RequestBody KhachHang khachHang) {
+    public String add(@RequestBody KhachHang khachHang) {
         khachHangService.addKhachHang(khachHang);
-        return ResponseEntity.ok(khachHang);
+        return "redirect :/admin/khachhang";
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<?> update(@PathVariable("id") Integer id, @RequestBody KhachHang khachHang) {
+    public String update(@PathVariable("id") Integer id, @RequestBody KhachHang khachHang) {
         khachHangService.updateKhachHang(id, khachHang);
-        return ResponseEntity.ok("update thanh cong");
+        return "redirect :/admin/khachhang";
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
+    public String delete(@PathVariable("id") Integer id) {
         khachHangService.deleteKhachHang(id);
-        return ResponseEntity.ok("delete thanh cong");
+        return "redirect :/admin/khachhang";
     }
 
 }
