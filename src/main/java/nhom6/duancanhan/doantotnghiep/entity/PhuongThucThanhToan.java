7cbdb4c383d07b8567fdd8a65760bc5ddf4218a2
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-10-11 20:36:12 +0700

    .

--- src/main/java/nhom6/duancanhan/doantotnghiep/entity/PhuongThucThanhToan.java
+++ src/main/java/nhom6/duancanhan/doantotnghiep/entity/PhuongThucThanhToan.java
@@ -17,8 +17,8 @@ public class PhuongThucThanhToan {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
-    @Column(name = "phuong_thuc")
-    private String phuongThuc;
+//    @Column(name = "phuong_thuc")
+//    private String phuongThuc;
     @Column(name = "trang_thai")
     private int trangThai;
     @Column(name = "phuong_thuc_thanh_toan")
