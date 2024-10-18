2666bd37e808d08ff865f363be3139d0143f864e
Storm-Whale <emphong481@gmail.com>
2024-10-18 08:14:42 +0700

    update_giao_dien

--- src/main/java/nhom6/duancanhan/doantotnghiep/controller/ClientController.java
+++ src/main/java/nhom6/duancanhan/doantotnghiep/controller/ClientController.java
@@ -5,6 +5,7 @@ import nhom6.duancanhan.doantotnghiep.service.service.SanPhamService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,13 +16,13 @@ public class ClientController {
     private final SanPhamService sanPhamService;
 
     @GetMapping("")
-    private String trangchuindex() {
+    private String trangchuindex(Model model) {
+        model.addAttribute("sanphams", sanPhamService.getAllSanPhamShowOnClient());
         return "/client/trangchu";
     }
 
-    @GetMapping("2")
-    private String trangchu2index(Model model) {
-        model.addAttribute("sanphams", sanPhamService.getAllSanPhamShowOnClient());
-        return "/client/trangchu2";
+    @GetMapping("/san_pham_chi_tiet/{id}")
+    private String sanPhamChiTiet(@PathVariable int id, Model model) {
+        return "/client/chitiet";
     }
 }
